e4593193710bc5d9c3387f0b3e176cdc4952e1ae
beangel128 <Benjamin.angel@mykenzie.snhu.edu>
2023-09-19 19:56:21 -0500

    environment.sh edit

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -1,8 +1,10 @@
 # Step One- Fill out the CAPSTONE_REPO_NAME and GITHUB_GROUP_NAME in all LOWERCASE.
-
+charactermodificationprogramlbc
+bugbusterslbc
 # Step Two - configure your shell to always have these variables.
 # For OSX / Linux
 # Copy and paste ALL of the properties below into your .bash_profile in your home directly
+
 
 # For Windows
 # Copy and paste ALL of the properties below into your .bashrc file in your home directory
@@ -14,11 +16,11 @@
 
 # Fill out the following values
 # The path of your repo on github. Don't include the whole URL, just the part after github.com/KenzieAcademy-SoftwareEngineering/
-export LBC_GROUP_NAME=replacethiswithyourgroupname
-export LBC_REPO_NAME=ata-lbc-project-$GITHUB_USERNAME
+export LBC_GROUP_NAME=bugbusterslbc
+export LBC_REPO_NAME=ata-lbc-project-beangel1218
 
 # Do not modify the rest of these unless you have been instructed to do so.
-export LBC_PROJECT_NAME=lbcproject
+export LBC_PROJECT_NAME=ata-lbc-project-beangel1218
 export LBC_PIPELINE_STACK=$LBC_PROJECT_NAME-$LBC_GROUP_NAME
 export LBC_ARTIFACT_BUCKET=$LBC_PROJECT_NAME-$LBC_GROUP_NAME-artifacts
 export LBC_DEPLOY_STACK=$LBC_PROJECT_NAME-$LBC_GROUP_NAME-application
