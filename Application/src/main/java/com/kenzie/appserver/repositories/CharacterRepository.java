ae5a2241ec8e477ad831e4eefda23d3140edfe40
beangel128 <Benjamin.angel@mykenzie.snhu.edu>
2023-10-06 10:09:15 -0500

    repo conlict fixed

--- Application/src/main/java/com/kenzie/appserver/repositories/CharacterRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/CharacterRepository.java
@@ -1,8 +1,6 @@
 package com.kenzie.appserver.repositories;
 
 import com.kenzie.appserver.repositories.model.CharacterRecord;
-//import com.kenzie.appserver.repositories.model.ExampleRecord;
-
 import com.kenzie.appserver.service.model.Character;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
