7bdda26001e1c9d5e4e4ba5567cf14a53fe4b972
Sara Merchan <sara.merchanagudelo@mykenzie.snhu.edu>
2023-10-07 21:02:54 -0400

    intTest

--- Application/src/test/java/com/kenzie/appserver/service/CharacterServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/CharacterServiceTest.java
@@ -1,14 +1,21 @@
 package com.kenzie.appserver.service;
 
+import com.kenzie.appserver.controller.model.CharacterResponse;
 import com.kenzie.appserver.repositories.CharacterRepository;
 import com.kenzie.appserver.repositories.model.CharacterRecord;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.kenzie.appserver.service.model.Character;
 import org.mockito.ArgumentCaptor;
-import org.testcontainers.shaded.com.google.common.base.Verify;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class CharacterServiceTest {
@@ -41,6 +48,13 @@ public class CharacterServiceTest {
         Assert.assertEquals(testCharacter.getHealthPoints(), savedRecord.getHealthPoints());
 
     }
+//    @Test
+//    void addNewCharacter_inputNull_throwsException(){
+//
+//    }
+
+
+
     @Test
     void updateCharacter(){
         Character testCharacter = new Character("sara", 50,
@@ -77,4 +91,72 @@ public class CharacterServiceTest {
 
     }
 
-}
+    @Test
+    void deleteCharacter_characterExist_deletionCompleted(){
+        //GIVEN
+        CharacterRecord existingCharacter = new CharacterRecord();
+        existingCharacter.setCharacter_name("Sara");
+        existingCharacter.setStrength(1);
+        existingCharacter.setDexterity(2);
+        existingCharacter.setSocial(3);
+        existingCharacter.setMagic(4);
+        existingCharacter.setMana(5);
+        existingCharacter.setHealthPoints(6);
+
+
+        doNothing().when(characterRepository).deleteById(existingCharacter.getCharacter_name());
+        //WHEN
+        characterService.deleteCharacter(existingCharacter.getCharacter_name());
+
+        //THEN
+//        verify(characterService.deleteCharacter(existingCharacter.getCharacter_name(),times(1));
+        assertFalse(characterRepository.existsById(existingCharacter.getCharacter_name()));
+    }
+        @Test
+        void findCharacterByID_IDExist_returnsCharacter(){
+           //GIVEN
+            CharacterRecord existingCharacter = new CharacterRecord();
+            existingCharacter.setCharacter_name("Sara");
+            existingCharacter.setStrength(1);
+            existingCharacter.setDexterity(2);
+            existingCharacter.setSocial(3);
+            existingCharacter.setMagic(4);
+            existingCharacter.setMana(5);
+            existingCharacter.setHealthPoints(6);
+
+
+            when(characterRepository.findById(existingCharacter.getCharacter_name()))
+                    .thenReturn(Optional.of(existingCharacter));
+            //WHEN
+            Character character = characterService.findByName(existingCharacter.getCharacter_name());
+
+            //THEN
+            assertEquals(character.getCharacter_name(),existingCharacter.getCharacter_name());
+        }
+
+        @Test
+        void findAllCharacters_validCharacterList_returnsList(){
+        //GIVEN
+            List<CharacterRecord> characterRecords = new ArrayList<>();
+            CharacterRecord existingCharacter = new CharacterRecord();
+            existingCharacter.setCharacter_name("Sara");
+            existingCharacter.setStrength(1);
+            existingCharacter.setDexterity(2);
+            existingCharacter.setSocial(3);
+            existingCharacter.setMagic(4);
+            existingCharacter.setMana(5);
+            existingCharacter.setHealthPoints(6);
+            characterRecords.add(existingCharacter);
+            when(characterRepository.findAll()).thenReturn(characterRecords);
+            //WHEN
+
+            List<Character> listOfCharacters = characterService.findAllCharacters();
+
+            //THEN
+            assertTrue(characterRecords.size() == 1);
+            assertEquals(listOfCharacters.size(), characterRecords.size());
+
+
+        }
+    }
+
--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/CharacterControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/CharacterControllerTest.java
@@ -1,12 +1,10 @@
 package com.kenzie.appserver.controller;
 
-import com.kenzie.appserver.IntegrationTest;
-import com.kenzie.appserver.controller.model.ExampleCreateRequest;
-import com.kenzie.appserver.service.ExampleService;
-import com.kenzie.appserver.service.model.Example;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kenzie.appserver.IntegrationTest;
+import com.kenzie.appserver.controller.model.CharacterCreateRequest;
+import com.kenzie.appserver.service.CharacterService;
 import net.andreinc.mockneat.MockNeat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,48 +19,55 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @IntegrationTest
-class CharacterControllerTest {
+public class CharacterControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Autowired
-    ExampleService exampleService;
-
+    CharacterService characterService;
     private final MockNeat mockNeat = MockNeat.threadLocal();
-
     private final ObjectMapper mapper = new ObjectMapper();
+    private final String CHARACTER_NAME = "NAME";
+//    @Test
+//    void getCharacter_returnsCharacter(){
+//     mvc.perform()
+
+//    @Test
+//    public void getById_Exists() throws Exception {
+//        String id = UUID.randomUUID().toString();
+//        String name = mockNeat.strings().valStr();
+//
+//        Character example = new CharacterService();
+//        Character persistedExample = characterService.addNewCharacter
+//                (example));
+//        mvc.perform(get("/example/{id}", persistedExample.getId())
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("id")
+//                        .value(is(id)))
+//                .andExpect(jsonPath("name")
+//                        .value(is(name)))
+//                .andExpect(status().isOk());
+//    }
 
     @Test
-    public void getById_Exists() throws Exception {
-        String id = UUID.randomUUID().toString();
+    public void createCharacter_CreateSuccessful() throws Exception {
         String name = mockNeat.strings().valStr();
 
-        Example example = new Example(id, name);
-        Example persistedExample = exampleService.addNewExample(example);
-        mvc.perform(get("/example/{id}", persistedExample.getId())
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("id")
-                        .value(is(id)))
-                .andExpect(jsonPath("name")
-                        .value(is(name)))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void createExample_CreateSuccessful() throws Exception {
-        String name = mockNeat.strings().valStr();
-
-        ExampleCreateRequest exampleCreateRequest = new ExampleCreateRequest();
-        exampleCreateRequest.setName(name);
-
+        CharacterCreateRequest characterCreateRequest = new CharacterCreateRequest();
+        characterCreateRequest.setCharacter_name(mockNeat.toString());
+        characterCreateRequest.setDexterity(mockNeat.ints().range(1,100).get());
+        characterCreateRequest.setHealthPoints(mockNeat.ints().range(1,100).get());
+        characterCreateRequest.setMana(mockNeat.ints().range(1,100).get());
+        characterCreateRequest.setMagic(mockNeat.ints().range(1,100).get());
+        characterCreateRequest.setStrength(mockNeat.ints().range(1,100).get());
+        characterCreateRequest.setSocial(mockNeat.ints().range(1,100).get());
         mapper.registerModule(new JavaTimeModule());
 
         mvc.perform(post("/example")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(exampleCreateRequest)))
+                        .content(mapper.writeValueAsString(characterCreateRequest)))
                 .andExpect(jsonPath("id")
                         .exists())
                 .andExpect(jsonPath("name")
