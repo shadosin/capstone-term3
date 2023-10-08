8f20ad7a3c1fed80a01d98f1a1a01756551676f6
beangel128 <Benjamin.angel@mykenzie.snhu.edu>
2023-10-08 18:43:14 -0500

    Unit test ile redone

--- Application/src/test/java/com/kenzie/appserver/service/CharacterServiceTest.java
+++ Application/src/test/java/com/kenzie/appserver/service/CharacterServiceTest.java
@@ -3,16 +3,24 @@ package com.kenzie.appserver.service;
 import com.kenzie.appserver.repositories.CharacterRepository;
 import com.kenzie.appserver.repositories.model.CharacterRecord;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.kenzie.appserver.service.model.Character;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.testcontainers.shaded.com.google.common.base.Verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
 public class CharacterServiceTest {
+
     private CharacterRepository characterRepository;
+    @Mock
     private  CharacterService characterService;
 
     @BeforeEach
@@ -22,59 +30,107 @@ public class CharacterServiceTest {
     }
 
     @Test
-    void add_newCharacter(){
-        Character testCharacter = new Character("sara", 50,
-                40,30,20,10,60);
-        ArgumentCaptor<CharacterRecord> captor = new ArgumentCaptor<>();
+    void deleteCharacter_characterExist_deletionCompleted(){
+        //GIVEN
+        CharacterRecord existingCharacter = new CharacterRecord();
+        existingCharacter.setCharacter_name("Sara");
+        existingCharacter.setStrength(1);
+        existingCharacter.setDexterity(2);
+        existingCharacter.setSocial(3);
+        existingCharacter.setMagic(4);
+        existingCharacter.setMana(5);
+        existingCharacter.setHealthPoints(6);
 
-        Character resultCharacter = characterService.addNewCharacter(testCharacter);
-        verify(characterRepository).save(captor.capture());
-        CharacterRecord savedRecord = captor.getValue();
 
-        Assert.assertEquals(testCharacter, resultCharacter);
-        Assert.assertEquals(testCharacter.getCharacter_name(), savedRecord.getCharacter_name());
-        Assert.assertEquals(testCharacter.getStrength(), savedRecord.getStrength());
-        Assert.assertEquals(testCharacter.getDexterity(), savedRecord.getDexterity());
-        Assert.assertEquals(testCharacter.getSocial(), savedRecord.getSocial());
-        Assert.assertEquals(testCharacter.getMagic(), savedRecord.getMagic());
-        Assert.assertEquals(testCharacter.getMana(), savedRecord.getMana());
-        Assert.assertEquals(testCharacter.getHealthPoints(), savedRecord.getHealthPoints());
+        doNothing().when(characterRepository).deleteById(existingCharacter.getCharacter_name());
+        //WHEN
+        characterService.deleteCharacter(existingCharacter.getCharacter_name());
+
+        //THEN
+        Assertions.assertFalse(characterRepository.existsById(existingCharacter.getCharacter_name()));
+    }
+    @Test
+    void findCharacterByID_IDExist_returnsCharacter(){
+        //GIVEN
+        CharacterRecord existingCharacter = new CharacterRecord();
+        existingCharacter.setCharacter_name("Sara");
+        existingCharacter.setStrength(1);
+        existingCharacter.setDexterity(2);
+        existingCharacter.setSocial(3);
+        existingCharacter.setMagic(4);
+        existingCharacter.setMana(5);
+        existingCharacter.setHealthPoints(6);
+
+
+        when(characterRepository.findById(existingCharacter.getCharacter_name()))
+                .thenReturn(Optional.of(existingCharacter));
+        //WHEN
+        Character character = characterService.findByName(existingCharacter.getCharacter_name());
+
+        //THEN
+        Assertions.assertEquals(character.getCharacter_name(),existingCharacter.getCharacter_name());
+    }
+
+    @Test
+    void findAllCharacters_validCharacterList_returnsList(){
+        //GIVEN
+        List<CharacterRecord> characterRecords = new ArrayList<>();
+        CharacterRecord existingCharacter = new CharacterRecord();
+        existingCharacter.setCharacter_name("Sara");
+        existingCharacter.setStrength(1);
+        existingCharacter.setDexterity(2);
+        existingCharacter.setSocial(3);
+        existingCharacter.setMagic(4);
+        existingCharacter.setMana(5);
+        existingCharacter.setHealthPoints(6);
+        characterRecords.add(existingCharacter);
+        when(characterRepository.findAll()).thenReturn(characterRecords);
+        //WHEN
+
+        List<Character> listOfCharacters = characterService.findAllCharacters();
+
+        //THEN
+        Assertions.assertTrue(characterRecords.size() == 1);
+        Assertions.assertEquals(listOfCharacters.size(), characterRecords.size());
+
 
     }
     @Test
-    void updateCharacter(){
-        Character testCharacter = new Character("sara", 50,
-                40,30,20,10,60);
-        ArgumentCaptor<CharacterRecord> captor = new ArgumentCaptor<>();
-        when(characterRepository.existsById("sara")).thenReturn(true);
-
-        characterService.updateCharacter(testCharacter);
-        verify(characterRepository).save(captor.capture());
-        verify(characterRepository).existsById("sara");
-        CharacterRecord savedRecord = captor.getValue();
-
-        Assert.assertEquals(testCharacter.getCharacter_name(), savedRecord.getCharacter_name());
-        Assert.assertEquals(testCharacter.getStrength(), savedRecord.getStrength());
-        Assert.assertEquals(testCharacter.getDexterity(), savedRecord.getDexterity());
-        Assert.assertEquals(testCharacter.getSocial(), savedRecord.getSocial());
-        Assert.assertEquals(testCharacter.getMagic(), savedRecord.getMagic());
-        Assert.assertEquals(testCharacter.getMana(), savedRecord.getMana());
-        Assert.assertEquals(testCharacter.getHealthPoints(), savedRecord.getHealthPoints());
-
+    void addNewCharacter_validCharacter_addSuccessful(){
+        //GIVEN
+        Character existingCharacter = new Character(
+                "Sara",
+                1,
+                2,
+                3,
+                4,
+                5,
+                6
+        );
+        //WHEN
+        Character character = characterService.addNewCharacter(existingCharacter);
+        //THEN
+        Assertions.assertEquals(character.getCharacter_name(),existingCharacter.getCharacter_name());
     }
-
     @Test
-    void updateCharacter_doesNotExist() {
-        Character testCharacter = new Character("sara", 50,
-                40, 30, 20, 10, 60);
+    void updateCharacter_characterExist_existingCharacterUpdated(){
+        //GIVEN
+        Character character = new Character(
+                "Sara",
+                2,
+                3,
+                4,
+                5,
+                6,
+                7
+        );
 
-        when(characterRepository.existsById("sara")).thenReturn(false);
+         when(characterRepository.existsById(character.getCharacter_name())).thenReturn(true);
+        //WHEN
+        characterService.updateCharacter(character);
 
-        characterService.updateCharacter(testCharacter);
-
-        verify(characterRepository).existsById("sara");
-        verify(characterRepository, times(0)).save(any());
+        //THEN
+        verify((characterRepository), times(2));
 
     }
-
 }
